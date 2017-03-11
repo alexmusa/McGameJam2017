720db95a6799ba56d871f2a62f32b71c77c71c21
Zetnir <ludovicbonheur@gmail.com>
2017-03-10 22:14:12 -0500

    Deplacement Personnage

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    private float x;
+    private float y;
+    public float Speed = 10f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        x = Input.GetAxis("Horizontal"); // premet de deplacer le personnage laterallement avec les fleches
+        y = Input.GetAxis("Vertical");
+        Mouvement(x, y);
+    }
+    void Mouvement(float x, float y)
+    {
+        Vector3 Deplacement = new Vector3(x * Speed * Time.deltaTime, 0, y * Speed * Time.deltaTime); // créé le deplacement
+        transform.Translate(Deplacement);// ***
+    }
+}
