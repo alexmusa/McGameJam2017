bd6a104c45956eecd7c00f125f9927ac29deb0ba
Zetnir <ludovicbonheur@gmail.com>
2017-03-10 23:07:42 -0500

    Deplacement Joueur 2
    plus de bug de collision et de gravité

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -3,23 +3,31 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
+
     private float x;
     private float y;
-    public float Speed = 10f;
+    public float Speed = 5f;
+    private Rigidbody rb;
+
+
 	// Use this for initialization
 	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
+        rb = gameObject.GetComponent<Rigidbody>();
+
+    }
+
+    // Update is called once per frame
+    void Update () {
         x = Input.GetAxis("Horizontal"); // premet de deplacer le personnage laterallement avec les fleches
-        y = Input.GetAxis("Vertical");
+        y = Input.GetAxis("Vertical");        
         Mouvement(x, y);
     }
     void Mouvement(float x, float y)
     {
-        Vector3 Deplacement = new Vector3(x * Speed * Time.deltaTime, 0, y * Speed * Time.deltaTime); // créé le deplacement
-        transform.Translate(Deplacement);// ***
+        Vector3 Deplacement = new Vector3(x * Speed, rb.velocity.y , y * Speed); // créé le deplacement
+        rb.velocity = Deplacement;
+
     }
+    
+   
 }
