633d575e0c36cd6c9fa2b45a5aa2e54b3767bcfe
alexmusa <alexandre.musaraganyi@polymtl.ca>
2017-03-11 08:56:28 -0500

    Debris destroyed after 10s

--- Assets/main.cs
+++ Assets/main.cs
@@ -4,22 +4,30 @@ using UnityEngine;
 
 public class main : MonoBehaviour {
 
-	//public GameObject enemy;                // The enemy prefab to be spawned.
-	//public float spawnTime = 3f;            // How long between each spawn.
-	//public Transform[] spawnPoints;         // An array of the spawn points this enemy can spawn from.
+    //public GameObject enemy;                // The enemy prefab to be spawned.
+    //public float spawnTime = 3f;            // How long between each spawn.
+    //public Transform[] spawnPoints;         // An array of the spawn points this enemy can spawn from.
+    private GameObject salle;
+
     private int nbPlayers = 4;
 
-    private Object joueur;
-    private Object debris;
+    private Object joueur; // Prefab du joueur
+    private Object debris; // Prefab du debris
+
+    private float timeSinceLastQuake = 0.0f;
 
     void Start ()
 	{
+        salle = GameObject.Find("Salle"); // Trouve la salle, l'objet servir ensuite à déclancher des Shake().
+
         joueur = Resources.Load("Joueur");
 
         for (int i = 0; i< nbPlayers; i++){
-            GameObject go = Instantiate(joueur, new Vector3(2 + i * 2, 1, 5), Quaternion.Euler(Vector3.zero)) as GameObject;
+            GameObject go = Instantiate(joueur, new Vector3(2 + i * (8/nbPlayers), 1, 5), Quaternion.Euler(Vector3.zero)) as GameObject;
             go.SendMessage("Create", i);
         }
+
+
 
         debris = Resources.Load("Debris");
         // Call the Spawn function after a delay of the spawnTime and then continue to call after the same amount of time.
@@ -29,7 +37,9 @@ public class main : MonoBehaviour {
 
     void Spawn ()
 	{
-        // Instancie un debris.
+        // Instancie un debris avec une durée de vie limitée.
+        float lifetime = 10.0f;
+
         Vector3 position = new Vector3(1.0f + 8.0f * Random.value,
                                        11.0f + 8.0f * Random.value,
                                        1.0f + 8.0f * Random.value);
@@ -38,12 +48,17 @@ public class main : MonoBehaviour {
                                                            360 * Random.value,
                                                            360 * Random.value));
 
-        Instantiate (debris, position, rotation);
+        Destroy(Instantiate (debris, position, rotation), lifetime);
 	}
 
     // Update is called once per frame
     void Update () {
-		
-	}
+        timeSinceLastQuake += Time.deltaTime;
+        if (timeSinceLastQuake > 5f)
+        {
+            timeSinceLastQuake -= 5f;
+            salle.SendMessage("Quake", new float[] { 1.0f, 3.0f });
+        }
+    }
 }
 
