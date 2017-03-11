812ae4eb1095c48ab36c94120b72787d0c94b13a
alexmusa <alexandre.musaraganyi@polymtl.ca>
2017-03-11 09:00:18 -0500

    Revert "Debris destroyed after 10s"
    This reverts commit 633d575e0c36cd6c9fa2b45a5aa2e54b3767bcfe.

---
+++ Assets/DebritController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DebritController : MonoBehaviour {
+	// Use this for initialization
+	void Start () {
+        Destroy(this, 4f);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        
+	}
+}
