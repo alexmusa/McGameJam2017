dd36a582f3587c5dad91e292127d22364d7132ad
Zetnir <ludovicbonheur@gmail.com>
2017-03-11 00:01:18 -0500

    Mouvement Joystick

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -1,26 +1,41 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 public class PlayerController : MonoBehaviour {
 
     private float x;
     private float y;
     public float Speed = 5f;
     private Rigidbody rb;
+    public float ForceSaut = 2f;
+    public GroundCheck groundcheck;
+    public bool AuSol;
 
 
-	// Use this for initialization
-	void Start () {
+
+
+    // Use this for initialization
+    void Start () {
         rb = gameObject.GetComponent<Rigidbody>();
+        groundcheck.GetComponent<GroundCheck>();
+
 
     }
 
     // Update is called once per frame
     void Update () {
-        x = Input.GetAxis("Horizontal"); // premet de deplacer le personnage laterallement avec les fleches
-        y = Input.GetAxis("Vertical");        
+        x = Input.GetAxis("Horizontal1"); // premet de deplacer le personnage laterallement avec les fleches
+        y = Input.GetAxis("Vertical1");        
         Mouvement(x, y);
+
+        AuSol = groundcheck.isGrounded;
+        if (AuSol)
+        {
+            Sauter();
+        }
+
+        Rotation();
+        
     }
     void Mouvement(float x, float y)
     {
@@ -28,6 +43,19 @@ public class PlayerController : MonoBehaviour {
         rb.velocity = Deplacement;
 
     }
-    
-   
+    void Sauter()
+    {
+        if (Input.GetKeyDown(KeyCode.Joystick1Button0))
+        {
+            Vector3 VecteurSaut = new Vector3(0f, ForceSaut, 0f);
+            rb.AddForce(VecteurSaut, ForceMode.Impulse);
+        }
+    }
+    void Rotation()
+    {
+        float rotate = Mathf.Atan(y / x);
+        transform.eulerAngles = new Vector3(0, rotate, 0);
+    }
+
+
 }
